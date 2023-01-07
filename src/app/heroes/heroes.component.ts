b89dee3f8a1247fd2baa11744af2d8018529bc34
YahyaRechaki <yahya.rechaki@gmail.com>
2023-01-07 17:53:51 +0100

    display a list

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HEROES} from '../mock-heroes'
 import { Hero } from '../hero';
 
 // @Component is a decorator function that specifies the Angular metadata for the component.
@@ -11,8 +12,11 @@ import { Hero } from '../hero';
 
 // Always export the component class so you can import it elsewhere … like in the AppModule.
 export class HeroesComponent {
-  hero : Hero = {
-    id: 1,
-    name: "Windstorm"
+  heroes = HEROES
+  chosenHero: any;
+  selectedHero?: Hero; // the "?" means that the selectedHero property may or may not have a value of type Hero. and if it does have a value, all of the properties in the Hero interface must be present and have the correct types.
+
+  onSelect(hero: any): void{
+    this.selectedHero = hero
   }
 }
